05ade6bf50604646e1cda543074a3ed0c89b4f26
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-01 18:00:39 +0530

    ignore the file

--- src/com/brigdelabz/MaximumNumber.java
+++ src/com/brigdelabz/MaximumNumber.java
@@ -3,8 +3,5 @@ package com.brigdelabz;
 public class MaximumNumber {
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum Number");
-        String str1 = "sahil";
-        String str2 = "hsds";
-        System.out.println(str1 == str2);
     }
 }
