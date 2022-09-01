48ba6a98ebadb03542ea2d09b8049f76f4dbd698
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-01 18:23:44 +0530

    Added: given 3 float find the maximum

--- src/com/brigdelabz/MaximumNumber.java
+++ src/com/brigdelabz/MaximumNumber.java
@@ -18,4 +18,21 @@ public class MaximumNumber {
             }
         }
     }
+    public static void floatsMax(float x, float y, float z) {
+        if (x > y) {
+            if (x > z) {
+                System.out.println("Maximum float is = " + x);
+            }
+        }
+        if (y > x) {
+            if (y > z) {
+                System.out.println("Maximum float is = " + y);
+            }
+        }
+        if (z > x) {
+            if (z > y) {
+                System.out.println("Maximum float is = " + z);
+            }
+        }
+    }
 }
--- src/com/brigdelabz/MaximumNumberMain.java
+++ src/com/brigdelabz/MaximumNumberMain.java
@@ -4,5 +4,6 @@ public class MaximumNumberMain {
     public static void main(String[] args) {
         System.out.println("welcome to Maximum Number");
         MaximumNumber.integerMax(56, 89, 56);
+        MaximumNumber.floatsMax(45.268F,63.89F,89.25F);
     }
 }
